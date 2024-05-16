17ab40cf056c8b3d97158333212788922c98e760
Haroon Hassan <91754484+Haroonrules@users.noreply.github.com>
2024-05-17 01:17:13 +0800

    update page.js
    added description and changed the logo

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -29,18 +29,27 @@ export default function Home() {
           isOpen ? "blur-sm " : ""
         } flex min-h-screen flex-col items-center justify-between bg-[#EEF5FF]`}
       >
-        <header className="mt-10">
-          <Image src={logo} width={140} height={50} />
+        <header className="mt-10 flex justify-center items-center">
+          <Image src={logo} width={140} height={50} />{" "}
+          <span className="font-bold text-2xl">
+            (Course resource management system)
+          </span>
         </header>
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-7xl text-center ">
             Your path to <span className="text-[#86B6F6]">success</span> <br />{" "}
             starts here
           </h1>
-          <p className="text-md text-center my-5">
-            Lorem ipsum dolor sit amet consectetur. Massa gravida viverra nulla
-            tortor eu dui.
-            <br /> Massa gravida viverra nulla tortor eu dui.
+          <p className="text-sm text-center my-5 w-1/2">
+            Welcome to our Course Resource Management System, where innovation
+            meets education. Leveraging blockchain technology, we provide a
+            secure and decentralized platform for students and teachers to
+            connect and share knowledge. Students can access a vast library of
+            courses, while teachers can upload and manage their resources with
+            ease. Our platform empowers learners to take control of their
+            educational journey, and enables educators to share their expertise
+            with a global audience.
+            <br /> Join us in revolutionizing the future of education!
           </p>
           <button
             className="bg-[#16191D] text-white my-4 px-8 py-3 rounded-full text-md"
